d4ebc984054981b071a850b9ad0185f6384bb070
HONGKAI123 <huanghongkai98@GMAIL.COM>
2022-03-29 02:59:33 -0700

    3/29

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Default from '../components/Default';
+import {Row , Col , Form , Input, Button} from 'antd'
+import FormItem from 'antd/lib/form/FormItem';
+import './login.css'
 
 function Login() {
   return (
@@ -8,7 +11,36 @@ function Login() {
       
       
       
-      <h1>LoginPage</h1>
+      <div className='login' >
+
+        <Row gutter={16} >
+          <Col lg={16}></Col>
+          <Col lg={8} >
+            <Form layout='vertical' className='login-box'>
+              <h1>Login to start your trip</h1>
+              <Form.Item name='username' label='Username' rules={[{required: true}]}>
+                             <Input/>
+                         </Form.Item>
+                <FormItem name ='password' label ='password' rules = {[{required: true}]}>
+                    <Input/>
+                </FormItem>
+
+                <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+              
+
+
+
+            </Form>
+            </Col>
+        </Row>
+
+
+
+
+
+      </div>
     
     
     
