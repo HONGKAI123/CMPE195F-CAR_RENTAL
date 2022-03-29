ac42a90e010dc7dd2910d27537055d28c8a89d9d
iamkennethhuang <kenneth.j.huang@sjsu.edu>
2022-03-28 18:51:00 -0700

    add button featueres

--- frontend/src/components/customNavbar.js
+++ frontend/src/components/customNavbar.js
@@ -17,14 +17,15 @@ export default function CustomNavbar(){
                     sx={{display: 'flex',
                         justifyContent: 'space-between'}}>
                     <Typography variant="h6" component="div" sx={{fontWeight: 700}}>
-                        <img src ='/logo3.png' alt='logo' 
-                        width='55px' height='30px'
-                        />
-                          YIYI Rental
+                        <img src ='/logo3.png' alt='logo' width='55px' height='30px'/>
+                        YIYI Rental
                     </Typography>
                     <Box>
-                        <Button color="inherit">Login</Button>
-                        <Button sx={{background: '#7E80DF', color: '#FFFFFF', ml: 3}}>Register</Button>
+                        <Button color="inherit" onClick={() => {history.push('/login')}}>Login</Button>
+                        <Button onClick={() => {history.push('/register')}}
+                            sx={{background: '#7E80DF', color: '#FFFFFF', ml: 3}}>
+                            Register
+                        </Button>
                     </Box>
                 </Toolbar>
             </AppBar>
