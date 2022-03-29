0d984956fccf1a907c070624cd5116df7b15dc83
iamkennethhuang <kenneth.j.huang@sjsu.edu>
2022-03-28 18:11:18 -0700

    mui app bar

--- frontend/src/components/customNavbar.js
+++ frontend/src/components/customNavbar.js
@@ -1,32 +1,30 @@
 import React, {useState} from 'react';
-import Button from 'react-bootstrap/Button';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import { useHistory } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export default function CustomNavbar(){
     let history = useHistory();
 
     return(
-        <Navbar>
-            <Container>
-                <Navbar.Brand href="/home">Car Rental</Navbar.Brand>
-                <Nav className="mx-0">
-                    <Button 
-                        onClick={()=> history.push('/login')}
-                        type='button'
-                        variant="secondary" 
-                        className="mx-2">
-                        Login
-                    </Button>
-                    <Button onClick={()=> history.push('/register')}
-                        type='button'>
-                        Register
-                    </Button>
-                </Nav>
-            </Container>
-        </Navbar>
+        <Box >
+            <AppBar position="static" sx={{background: '#333333'}}>
+                <Toolbar 
+                    sx={{display: 'flex',
+                        justifyContent: 'space-between'}}>
+                    <Typography variant="h6" component="div" sx={{fontWeight: 700}}>
+                        YIYI Rental
+                    </Typography>
+                    <Box>
+                        <Button color="inherit">Login</Button>
+                        <Button sx={{background: '#7E80DF', color: '#FFFFFF', ml: 3}}>Register</Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
     
 }
