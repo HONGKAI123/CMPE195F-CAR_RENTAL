0aeea4d908ac9ff5e70a68faf0c893411022c83e
HONGKAI123 <huanghongkai98@GMAIL.COM>
2022-03-29 23:57:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = 5000
 const dbConnection = require('./db')
 app.get('/',(req,res) => res.send('caonima'))
 
-app.listen(port,() => console.log('nodejs in running'))
+app.listen(port,() => console.log('nodejs in running on port 5000'))
