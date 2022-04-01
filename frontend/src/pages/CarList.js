623f4b3ccb7bd82416d2d0f86fef88332f827efe
HONGKAI123 <huanghongkai98@GMAIL.COM>
2022-03-31 17:14:10 -0700

    updated carlist page and login

--- frontend/src/pages/CarList.js
+++ frontend/src/pages/CarList.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Default from '../components/Default';
 
 export default function CarList(){
     return (
+    <Default>
         <h1>Car List</h1>
+        </Default>
     );
 }
