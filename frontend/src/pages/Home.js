0aa7f26eacb4a9c52f7a35bf042c80902df52fb3
HONGKAI123 <huanghongkai98@GMAIL.COM>
2022-04-09 00:23:43 -0700

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -16,8 +16,8 @@ function Home() {
          
          <img src = "./backg.jpg"  />
          
-          <div class='text1'><h1 class="animate__animated animate__backInDown animate__delay-2s">Start Looking for your dream car!{' '}</h1>
-             <div><a href='./Login' className='learn'><h2 class = "animate__animated animate__backInUp animate__delay-3s">Learn More</h2></a></div>
+          <div class='text1'><h1 class="animate__animated animate__backInDown animate__delay-1s">Start Looking for your dream car!{' '}</h1>
+             <div><a href='./Login' className='learn'><h2 class = "animate__animated animate__backInUp animate__delay-2s">Learn More</h2></a></div>
 
 
           </div>
